bc9f5c3140c15bf02f9c72b8d754e515eb679c27
Petrik Diak <diak@petrik.hu>
2023-09-28 08:32:52 +0200

    Téglalap osztály létrehozása, b oldal létrehozása, ősosztály meghívása

---
+++ SokszogOOP/Teglalap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SokszogOOP
+{
+	internal class Teglalap : Sokszog
+	{
+		private double b;
+
+		public Teglalap(double a,double b) : base(a)
+		{
+			this.b = b;
+		}
+	}
+}
