924118128c8db3faeb1d5339b1cc01b8bae89947
Petrik Diak <diak@petrik.hu>
2023-09-28 09:22:32 +0200

    Program.cs-ben háromszögek, és téglalapok létrehozása és kiírása

--- SokszogOOP/Program.cs
+++ SokszogOOP/Program.cs
@@ -10,6 +10,15 @@ namespace SokszogOOP
 	{
 		static void Main(string[] args)
 		{
+			Haromszog haromszog = new Haromszog();
+			Haromszog haromszog2 = new Haromszog(3, 4, 5);
+			Teglalap teglalap = new Teglalap(3, 5);
+			Teglalap teglalap2 = new Teglalap(4, 2);
+
+			Console.WriteLine(haromszog);
+			Console.WriteLine(haromszog2);
+			Console.WriteLine(teglalap);
+			Console.WriteLine(teglalap2);
 		}
 	}
 }
